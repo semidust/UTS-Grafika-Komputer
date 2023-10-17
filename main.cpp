dc50ba3033b166bdd8458c3bda49156b77df7eb7
kambingcute <agathasinaga1@gmail.com>
2023-10-17 23:35:35 +0700

    gl-3
    index buffer

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -24,26 +24,44 @@ int main(void)
 
     GLenum err = glewInit();
 
+    // data yg dikirim ke GPU
+    float vertices[] = {
+        -1, 1,
+        -1, 0.8,
+        0.8, 0.8,
+        0.8, 1,
+        1, 1,
+        1, -1,
+        0.8, -1,
+        0,8, -0.8,
+        -1, -0.8,
+        -1, -1
+    };
+
+    unsigned indexArr[] = {
+        0, 1, 2, 
+        0, 2, 3,
+        3, 6, 5,
+        3, 5, 4,
+        8, 6, 7,
+        8, 9, 6,
+    };
+
     unsigned int vertexBuffer = 0;
     glGenBuffers(1, &vertexBuffer);
 
-    std:: cout << vertexBuffer << std::endl;
-
     // informasi penggunaan buffer ke GPU 
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-
-    // data yg dikirim ke GPU
-    float vertices[6] = {
-        0.0f, 0.5f,
-        -0.5f, -0.5f,
-        0.5f, -0.5f
-    };
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, vertices, GL_STATIC_DRAW); // vertices = tempat data itu diambil
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 20, vertices, GL_STATIC_DRAW); // vertices = tempat data itu diambil
     
     // instruksi ke GPU
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // index, brp dimensi (2d), tipe data, normalized, jarak antar vertex, pada index keberapa dimulai)
+
+    unsigned int indexBuffer = 0;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(unsigned int) * 18, indexArr, GL_STATIC_DRAW);
 
     string vertexString = readFile("vertex.vert");
     string fragmentString = readFile("fragment.frag");
@@ -72,8 +90,9 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(program);
-        glDrawArrays(GL_TRIANGLES, 0, 3); // ada 3 vertex
+        //glDrawArrays(GL_TRIANGLES, 0, 6); // ada 3 vertex
 
+        glDrawElements(GL_TRIANGLES, 18,GL_UNSIGNED_INT, nullptr);
         /*
         glBegin(GL_TRIANGLES); 
         glVertex2f(0.0f, 0.5f);
