11424ae3b69b905496d6b10805f5e9fbd6767f29
semionc <x.sammythagi@gmail.com>
2023-10-18 22:52:33 +0700

    tutor4

--- fragment.frag
+++ fragment.frag
@@ -1,6 +1,18 @@
+uniform vec4 color1;
+uniform vec4 color2;
+
+
 layout (location = 0) out vec4 color;
+varying vec2 vPos;
 
 void main()
 {
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	if (vPos.y < 0)
+	{
+		color = color1;
+	}
+	else
+	{
+		color = color2;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <math.h>
 #include "util.h"
+
+float color1[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+float color2[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+
+unsigned int program;
+GLint color1Loc, color2Loc;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_C && action == GLFW_PRESS)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            color1[i] = (rand() * 1.0f) / RAND_MAX;
+            color2[i] = (rand() * 1.0f) / RAND_MAX;
+        }
+
+        std::cout << color1[0] << " " << color1[1] << " " << color1[2] << " " << color1[3] << std::endl;
+        std::cout << color2[0] << " " << color2[1] << " " << color2[2] << " " << color2[3] << std::endl;
+
+        glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
+        glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
+    }
+}
 
 int main(void)
 {
@@ -22,6 +47,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSetKeyCallback(window, keyCallback);
+
     GLenum err = glewInit();
 
     // data yg dikirim ke GPU
@@ -38,13 +65,13 @@ int main(void)
         -1, -1
     };
 
-    unsigned int indexArr[] = {
+    unsigned indexArr[] = {
         0, 1, 2,
         0, 2, 3,
         3, 6, 5,
         3, 5, 4,
         8, 6, 7,
-        8, 9, 6
+        8, 9, 6,
     };
 
     unsigned int vertexBuffer = 0;
@@ -78,10 +105,20 @@ int main(void)
     glCompileShader(vertexShader);
     glCompileShader(fragmentShader);
 
-    unsigned int program = glCreateProgram();
+    program = glCreateProgram();
     glAttachShader(program, vertexShader);
     glAttachShader(program, fragmentShader);
     glLinkProgram(program);
+
+    glUseProgram(program);
+    GLint scaleLoc = glGetUniformLocation(program, "scale");
+    glUniform1f(scaleLoc, 0.5f);
+
+    color1Loc = glGetUniformLocation(program, "color1");
+    glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
+
+    color2Loc = glGetUniformLocation(program, "color2");
+    glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
