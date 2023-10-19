b6908faa93f0e4d730ac4770a67eb685a5364417
semionc <x.sammythagi@gmail.com>
2023-10-20 00:49:04 +0700

    update

--- main.cpp
+++ main.cpp
@@ -142,7 +142,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 640, "UTS - Pinkie Pie", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
