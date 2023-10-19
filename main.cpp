14d03fd56c836132eb658c96f84dc491b6a364e6
semionc <x.sammythagi@gmail.com>
2023-10-19 11:39:02 +0700

    gl-05

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 float color1[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 float color2[] = { 1.0f, 0.0f, 0.0f, 1.0f };
 
-unsigned int program;
+/*unsigned int program;
 GLint color1Loc, color2Loc;
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -26,7 +26,7 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
         glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
         glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
     }
-}
+}*/
 
 int main(void)
 {
@@ -47,48 +47,39 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    glfwSetKeyCallback(window, keyCallback);
-
     GLenum err = glewInit();
 
     // data yg dikirim ke GPU
     float vertices[] = {
-        -1, 1,
-        -1, 0.8,
-        0.8, 0.8,
-        0.8, 1,
-        1, 1,
-        1, -1,
-        0.8, -1,
-        0.8, -0.8,
-        -1, -0.8,
-        -1, -1
+       1.0, 1.0,
+       -1.0, 1.0,
+       -1.0, -1.0,
+       1.0, -1.0
     };
 
     unsigned indexArr[] = {
-        0, 1, 2,
-        0, 2, 3,
-        3, 6, 5,
-        3, 5, 4,
-        8, 6, 7,
-        8, 9, 6,
+        0, 1, 2, 0, 2, 3
     };
 
     unsigned int vertexBuffer = 0;
-    glGenBuffers(1, &vertexBuffer);
 
+    glGenBuffers(1, &vertexBuffer);
     // informasi penggunaan buffer ke GPU 
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 20, vertices, GL_STATIC_DRAW); // vertices = tempat data itu diambil
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 8, vertices, GL_STATIC_DRAW); // vertices = tempat data itu diambil
+
+    unsigned int indexBuffer = 0;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * 6, indexArr, GL_STATIC_DRAW);
 
     // instruksi ke GPU
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0); // index, brp dimensi (2d), tipe data, normalized, jarak antar vertex, pada index keberapa dimulai)
 
-    unsigned int indexBuffer = 0;
-    glGenBuffers(1, &indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * 18, indexArr, GL_STATIC_DRAW);
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
     string vertexString = readFile("vertex.vert");
     string fragmentString = readFile("fragment.frag");
@@ -96,29 +87,21 @@ int main(void)
     const char* vertexChar = vertexString.c_str();
     const char* fragmentChar = fragmentString.c_str();
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
     glShaderSource(vertexShader, 1, &vertexChar, NULL);
     glShaderSource(fragmentShader, 1, &fragmentChar, NULL);
 
     glCompileShader(vertexShader);
     glCompileShader(fragmentShader);
 
-    program = glCreateProgram();
+    unsigned int program = glCreateProgram();
     glAttachShader(program, vertexShader);
     glAttachShader(program, fragmentShader);
+
     glLinkProgram(program);
 
-    glUseProgram(program);
-    GLint scaleLoc = glGetUniformLocation(program, "scale");
-    glUniform1f(scaleLoc, 0.5f);
+    /*GLint scaleLoc = glGetUniformLocation(program, "scale");
+    glProgramUniform1f(program, scaleLoc, 0.5f);*/
 
-    color1Loc = glGetUniformLocation(program, "color1");
-    glUniform4f(color1Loc, color1[0], color1[1], color1[2], color1[3]);
-
-    color2Loc = glGetUniformLocation(program, "color2");
-    glUniform4f(color2Loc, color2[0], color2[1], color2[2], color2[3]);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -129,7 +112,7 @@ int main(void)
         glUseProgram(program);
         //glDrawArrays(GL_TRIANGLES, 0, 6); // ada 3 vertex
 
-        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, nullptr);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         /*
         glBegin(GL_TRIANGLES);
         glVertex2f(0.0f, 0.5f);
